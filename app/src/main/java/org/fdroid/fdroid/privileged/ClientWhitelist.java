ac374cd3cd2dfc0e6ea3e024f08847e7915a118a
Chirayu Desai <chirayudesai1@gmail.com>
2019-02-10 16:21:11 +0530

    Whitelist CalyxOS key
    * Also drop the official and temporary keys, since it's a system app
      there can only be one version installed.

--- app/src/main/java/org/fdroid/fdroid/privileged/ClientWhitelist.java
+++ app/src/main/java/org/fdroid/fdroid/privileged/ClientWhitelist.java
@@ -34,9 +34,8 @@ import java.util.HashSet;
 public class ClientWhitelist {
 
     public static HashSet<Pair<String, String>> whitelist = new HashSet<>(Arrays.asList(
-            // certificate SHA-256 of https//f-droid.org/F-Droid.apk
-            new Pair<>("org.fdroid.fdroid", "43238d512c1e5eb2d6569f4a3afbf5523418b82e0a3ed1552770abb9a9c9ccab"),
-            new Pair<>("org.fdroid.fdroid", "a5f4b40068a92ed4528177de1050fc6961fa44e75f506ec97fa6d3ef4b99b8e7") // temporary key, aspire
+            // certificate SHA-256 of /system/app/F-Droid/F-Droid.apk
+            new Pair<>("org.fdroid.fdroid", "3bb5073980761a42bf81375d033d74dba544d7e05a143c1b6cb5fea0c5e1b4f8")
     ));
 
 }
