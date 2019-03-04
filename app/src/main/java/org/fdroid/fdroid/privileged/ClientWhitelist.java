7785c18609a60acdcefa4bd0c3a0c09330dcf3fa
Chirayu Desai <chirayudesai1@gmail.com>
2019-03-04 17:06:50 +0530

    Keep F-Droid presigned

--- app/src/main/java/org/fdroid/fdroid/privileged/ClientWhitelist.java
+++ app/src/main/java/org/fdroid/fdroid/privileged/ClientWhitelist.java
@@ -34,8 +34,8 @@ import java.util.HashSet;
 public class ClientWhitelist {
 
     public static HashSet<Pair<String, String>> whitelist = new HashSet<>(Arrays.asList(
-            // certificate SHA-256 of /system/app/F-Droid/F-Droid.apk
-            new Pair<>("org.fdroid.fdroid", "3bb5073980761a42bf81375d033d74dba544d7e05a143c1b6cb5fea0c5e1b4f8")
+            // certificate SHA-256 of https//f-droid.org/F-Droid.apk
+            new Pair<>("org.fdroid.fdroid", "43238d512c1e5eb2d6569f4a3afbf5523418b82e0a3ed1552770abb9a9c9ccab")
     ));
 
 }
