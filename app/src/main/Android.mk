de686bbc745b02b5cf9396d2b42cc00bb21c7f9f
Michael Bestas <mkbestas@gmail.com>
2021-12-17 01:23:45 +0200

    Move to /product
    Change-Id: I4f281e755d23f0fb8f272ce5176a3a31df62e578

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := permissions_org.fdroid.fdroid.privileged.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
@@ -12,6 +12,7 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := F-DroidPrivilegedExtension
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRODUCT_MODULE := true
 LOCAL_SDK_VERSION := current
 # Keep IPackageInstallObserver and IPackageDeleteObserver
 LOCAL_PROGUARD_ENABLED := disabled
